bc70f35251bc74c69010b8b1027bd8bd7077e56a
ardilalukita14 <lukitasari623@gmail.com>
2022-06-11 13:43:02 +0700

    Message broker dashboard modifikasi

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -85,43 +85,128 @@
             client.connect(options);
         };
         //this adds the plots to the chart
-        function plot(point, chartno) {
-            console.log(point);
+        // function plot(point, chartno) {
+        //     console.log(point);
 
-            var series = chart.series[0],
-                shift = series.data.length > 20; // shift if the series is
-            // longer than 20
-            // add the point
-            chart.series[chartno].addPoint(point, true, shift);
-        };
+        //     var series = chart.series[0],
+        //         shift = series.data.length > 20; // shift if the series is
+        //     // longer than 20
+        //     // add the point
+        //     chart.series[chartno].addPoint(point, true, shift);
+        // };
         //settings for the chart
         $(document).ready(function() {
-            chart = new Highcharts.Chart({
-                chart: {
-                    renderTo: 'container',
-                    defaultSeriesType: 'spline'
-                },
-                title: {
-                    text: 'Dashboard IoT JTI - Suhu Live Websockets'
-                },
-                subtitle: {
-                    text: 'broker: ' + MQTTbroker + ' | port: ' + MQTTport + ' | topic : ' + MQTTsubTopic
-                },
-                xAxis: {
-                    type: 'datetime',
-                    tickPixelInterval: 50,
-                    maxZoom: 20 * 1000
-                },
-                yAxis: {
-                    minPadding: 0.2,
-                    maxPadding: 0.2,
-                    title: {
-                        text: 'Value',
-                        margin: 80
-                    }
-                },
-                series: []
-            });
+            var gaugeOptions = {
+    chart: {
+        type: 'solidgauge'
+    },
+
+    title: null,
+
+    pane: {
+        center: ['50%', '85%'],
+        size: '140%',
+        startAngle: -90,
+        endAngle: 90,
+        background: {
+            backgroundColor:
+                Highcharts.defaultOptions.legend.backgroundColor || '#EEE',
+            innerRadius: '60%',
+            outerRadius: '100%',
+            shape: 'arc'
+        }
+    },
+
+    exporting: {
+        enabled: false
+    },
+
+    tooltip: {
+        enabled: false
+    },
+
+    // the value axis
+    yAxis: {
+        stops: [
+            [0.1, '#55BF3B'], // green
+            [0.5, '#DDDF0D'], // yellow
+            [0.9, '#DF5353'] // red
+        ],
+        lineWidth: 0,
+        tickWidth: 0,
+        minorTickInterval: null,
+        tickAmount: 2,
+        title: {
+            y: -70
+        },
+        labels: {
+            y: 16
+        }
+    },
+
+    plotOptions: {
+        solidgauge: {
+            dataLabels: {
+                y: 5,
+                borderWidth: 0,
+                useHTML: true
+            }
+        }
+    }
+};
+
+// The speed gauge
+var chartSpeed = Highcharts.chart('container-speed', Highcharts.merge(gaugeOptions, {
+    yAxis: {
+        min: 0,
+        max: 100,
+        title: {
+            text: 'Suhu'
+        }
+    },
+
+    credits: {
+        enabled: false
+    },
+
+    series: [{
+        name: 'Suhu',
+        dataLabels: {
+            format:
+                '<div style="text-align:center">' +
+                '<span style="font-size:25px">{y}</span><br/>' +
+                '<span style="font-size:12px;opacity:0.4">km/h</span>' +
+                '</div>'
+        },
+        tooltip: {
+            valueSuffix: ' C'
+        }
+    }]
+
+}));
+
+
+// Bring life to the dials
+setInterval(function () {
+    // Speed
+    var point,
+        newVal,
+        inc;
+
+    if (chartSpeed) {
+        point = chartSpeed.series[0].points[0];
+        inc = Math.round((Math.random() - 0.5) * 100);
+        newVal = point.y + inc;
+
+        if (newVal < 0 || newVal > 200) {
+            newVal = point.y - inc;
+        }
+
+        point.update(newVal);
+    }
+
+}, 2000);
+
         });
     </script>
     <script src="http://code.highcharts.com/stock/highstock.js"></script>
